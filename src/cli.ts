2876e68f63cd664e680811699f90429d56e12588
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-09-28 14:23:59 +0800

    fix/cli: Make it work
    Fixes #22

--- src/cli.ts
+++ src/cli.ts
@@ -1,59 +1,15 @@
 #!/usr/bin/env node
 
-import path from 'path';
-import yargs from 'yargs';
-import {savePrecomputedDB} from '../lib/server';
-import * as compute from './commands/compute';
+import * as yargs from 'yargs';
 
-// TODO: Move this code into the compute module.
-const parser = yargs
-  .command(
-    compute.command,
-    compute.desc,
-    yargs =>
-      yargs
-        .options({
-          output: {
-            describe: 'Path to output computed database to',
-            type: 'string',
-            default: path.resolve(
-              path.join(process.cwd(), 'public', 'db.json')
-            ),
-            alias: 'o',
-          },
-          model: {
-            describe: 'Path to model (must contain model.json and vocab.json)',
-            type: 'string',
-            default: path.resolve(path.join(process.cwd(), 'public', 'sbert')),
-            alias: 'm',
-          },
-        })
-        .positional('dataset', {
-          describe: 'Path to the Geolexica concepts dataset',
-          type: 'string',
-          demandOption: true,
-        }),
-    argv => {
-      process.stdout.write(
-        `[breviter] Computing DB concept dataset at path "${argv.dataset}" ...`
-      );
-      savePrecomputedDB(argv.dataset, argv.output, argv.model).then(d => {
-        process.stdout.write(
-          `[breviter] Written precomputed DB to path "${argv.output}".`
-        );
-        // process.exit(0);
-      });
-    }
-  )
+import {command, desc, builder, handler} from './commands/compute';
+
+// Do not exit process until the save is finished!
+yargs
+  .command(command, desc, builder, handler)
   .help()
   .demandCommand()
-  .fail(false);
+  .fail(false)
+  .parseSync(); //.fail(err => { console.info(`${err.message}\n ${parser.getHelp()}`); });
 
-// TODO: Do not exit process until the save is finished!
-// try {
-//   const argv = await parser.parse();
-// } catch (err: any) {
-//   console.info(`${err.message}\n ${await parser.getHelp()}`)
-// }
-// console.info('finish')
-parser.parse();
+console.info('finish');
--- src/commands/compute.ts
+++ src/commands/compute.ts
@@ -1,51 +1,50 @@
-// import path from 'path';
+import * as path from 'path';
 import '@tensorflow/tfjs-node';
-// import {savePrecomputedDB} from '../../lib/server';
-// import type {ArgumentsCamelCase, CommandBuilder} from 'yargs';
+import {savePrecomputedDB} from '../../lib/server';
+import type {ArgumentsCamelCase, CommandBuilder} from 'yargs';
+import * as yargs from 'yargs';
 
 export const command = 'compute <dataset>';
 export const desc =
   'Compute semantic database for concept dataset at <dataset>';
 
-// TODO: Somehow I can't get yargs to read the "builder" and "handler" functions...
+export type Options = {
+  dataset: string;
+  output: string;
+  model: string;
+};
 
-// export type Options = {
-//   dataset: string;
-//   output: string;
-//   model: string;
-// };
+export const builder = (yargs: yargs.Argv<{}>) => {
+  return yargs
+    .options({
+      output: {
+        describe: 'Path to output computed database to',
+        type: 'string',
+        default: path.resolve(path.join(process.cwd(), 'public', 'db.json')),
+        alias: 'o',
+      },
+      model: {
+        describe: 'Path to model (must contain model.json and vocab.json)',
+        type: 'string',
+        default: path.resolve(path.join(process.cwd(), 'public', 'sbert')),
+        alias: 'm',
+      },
+    })
+    .positional('dataset', {
+      describe: 'Path to the Geolexica concepts dataset',
+      type: 'string',
+      demandOption: true,
+    });
+};
 
-// export function builder (yargs: any) {
-//   return yargs
-//     .options({
-//       output: {
-//         describe: 'Path to output computed database to',
-//         type: 'string',
-//         default: path.resolve(path.join(process.cwd(), 'public', 'db.json')),
-//         alias: 'o',
-//       },
-//       model: {
-//         describe: 'Path to model (must contain model.json and vocab.json)',
-//         type: 'string',
-//         default: path.resolve(path.join(process.cwd(), 'public', 'sbert')),
-//         alias: 'm',
-//       },
-//     })
-//     .positional('dataset', {
-//       describe: 'Path to the Geolexica concepts dataset',
-//       type: 'string',
-//       demandOption: true
-//     });
-// };
-
-// export function handler (argv: ArgumentsCamelCase<Options>) {
-//   const { dataset, output, model } = argv;
-//   console.log("hello!")
-//   process.stdout.write(`[breviter] Computing DB concept dataset at ${dataset} ...`);
-//   savePrecomputedDB(dataset, output, model).then(
-//     d => {
-//       process.stdout.write(`[breviter] Written precomputed DB to ${output}.`);
-//       process.exit(0);
-//     }
-//   );
-// };
+export const handler = (argv: ArgumentsCamelCase<Options>) => {
+  const {dataset, output, model} = argv;
+  console.log('cli handler');
+  process.stdout.write(
+    `[breviter] Computing DB concept dataset at ${dataset} ...`
+  );
+  savePrecomputedDB(dataset, output, model).then(d => {
+    process.stdout.write(`[breviter] Written precomputed DB to ${output}.`);
+    process.exitCode = 0;
+  });
+};
